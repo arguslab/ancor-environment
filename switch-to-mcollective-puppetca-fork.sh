9e802b8416ca8242af90164ed7eaf8a06c1e5700
Ian Unruh <ianunruh@gmail.com>
2014-03-13 12:16:28 -0500

    Switched to mcollective-puppetca-agent fork

---
+++ switch-to-mcollective-puppetca-fork.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -x -e
+
+MCO_PLUGINS=/usr/share/mcollective/plugins/mcollective
+CLONE_PATH=/tmp/mcollective-puppetca-agent
+
+rm -rf $CLONE_PATH
+rm -rf $MCO_PLUGINS/agent/puppetca*
+rm -rf $MCO_PLUGINS/util/puppetca*
+
+git clone git://github.com/ianunruh/mcollective-puppetca-agent.git $CLONE_PATH
+cp -r $CLONE_PATH/agent $MCO_PLUGINS
+cp -r $CLONE_PATH/util $MCO_PLUGINS
+
+rm -r $CLONE_PATH
+
+service mcollective restart
